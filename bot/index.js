2825a7bb3a7f550d86fa3353aff8411226469d39
DSQD-1 <dsq_d_tg@bk.ru>
2026-07-23 22:19:02 +0300

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -1,32 +1,130 @@
-const { Telegraf } = require("telegraf");
+const { Telegraf, Markup } = require("telegraf");
 require("dotenv").config();
+
+const {
+  createGame,
+  joinGame,
+  startGame,
+  getGames
+} = require("./game");
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const ADMIN_ID = 5082864281;
 
+
 bot.start((ctx) => {
-  const id = ctx.from.id;
 
-  let text = `
-🎭 Добро пожаловать в Mafiai!
+  const buttons = [
+    [
+      Markup.button.callback(
+        "🎮 Играть",
+        "play"
+      )
+    ],
+    [
+      Markup.button.callback(
+        "👤 Профиль",
+        "profile"
+      )
+    ]
+  ];
 
-Игра в мафию прямо в Telegram.
 
-Доступно:
-🎮 Играть
-👤 Профиль
-🏆 Рейтинг
-⭐ Магазин
-`;
-
-  if (id === ADMIN_ID) {
-    text += "\n\n👑 Вам доступна админ-панель";
+  if (ctx.from.id === ADMIN_ID) {
+    buttons.push([
+      Markup.button.callback(
+        "👑 Админка",
+        "admin"
+      )
+    ]);
   }
 
-  ctx.reply(text);
+
+  ctx.reply(
+`🎭 Mafiai
+
+Добро пожаловать в игру мафия!
+
+Создавай комнаты,
+получай роли,
+играй с друзьями.`,
+    Markup.inlineKeyboard(buttons)
+  );
 });
+
+
+
+bot.action("play", (ctx)=>{
+
+  const game = createGame(
+    ctx.from.id,
+    10
+  );
+
+  joinGame(
+    game.id,
+    {
+      id: ctx.from.id,
+      name: ctx.from.first_name
+    }
+  );
+
+
+  ctx.reply(
+`🎮 Лобби создано!
+
+ID игры:
+${game.id}
+
+Игроков:
+1/${game.maxPlayers}
+
+Ожидаем игроков...`
+  );
+
+});
+
+
+
+bot.action("profile",(ctx)=>{
+
+ctx.reply(
+`👤 Профиль
+
+Имя:
+${ctx.from.first_name}
+
+ID:
+${ctx.from.id}
+
+⭐ Рейтинг:
+0/5`
+);
+
+});
+
+
+
+bot.action("admin",(ctx)=>{
+
+ctx.reply(
+`👑 Админ-панель Mafiai
+
+Доступно:
+- Пользователи
+- Игры
+- Роли
+- Режимы`
+);
+
+});
+
+
 
 bot.launch();
 
-console.log("Mafiai запущен!");
+console.log(
+"🎭 Mafiai работает"
+);
