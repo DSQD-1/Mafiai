54175e86aeaa954a31a5872d2b28cd19b103192d
DSQD-1 <dsq_d_tg@bk.ru>
2026-07-24 07:29:08 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,306 +3,149 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const { giveRoles } = require("./roles");
-
 const {
-createGame,
-nightAction,
-startDay,
-vote,
-getWinner
+  createGame,
+  nightAction,
+  startDay,
+  vote,
+  getWinner
 } = require("./game");
-
 
 const app = express();
 
 app.use(express.json());
 
-
 const server = http.createServer(app);
 
-
-const io = new Server(server,{
-cors:{
-origin:"*"
-}
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
 });
-
 
 const rooms = {};
 
+// Проверка, что сервер работает
+app.get("/", (req, res) => {
+  res.send("🎭 Mafiai Server Online");
+});
 
-// создание комнаты через Mini App
+// Создание комнаты
+app.post("/create-room", (req, res) => {
+  try {
+    console.log("POST /create-room");
+    console.log(req.body);
 
-app.post("/create-room",(req,res)=>{
+    const name = req.body?.name || "Игрок";
 
+    const code = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
 
-const name = req.body.name;
+    rooms[code] = {
+      players: [
+        {
+          id: null,
+          name,
+          role: null,
+          alive: true
+        }
+      ],
+      status: "waiting"
+    };
 
+    res.status(200).json({
+      success: true,
+      code
+    });
 
-const code = Math.floor(
-100000 + Math.random()*900000
-).toString();
+  } catch (err) {
+    console.error(err);
 
+    res.status(500).json({
+      success: false,
+      error: err.message
+    });
+  }
+});
 
+io.on("connection", (socket) => {
 
-rooms[code]={
+  console.log("Игрок подключился", socket.id);
 
-players:[{
+  socket.on("joinRoom", (data) => {
 
-id:null,
+    const room = data.room;
 
-name:name,
+    if (!rooms[room]) {
+      rooms[room] = {
+        players: [],
+        status: "waiting"
+      };
+    }
 
-role:null,
+    rooms[room].players.push({
+      id: socket.id,
+      name: data.name,
+      role: null,
+      alive: true
+    });
 
-alive:true
+    socket.join(room);
 
-}],
+    io.to(room).emit(
+      "players",
+      rooms[room].players.map(p => p.name)
+    );
 
+  });
 
-status:"waiting"
+  socket.on("startGame", (room) => {
 
-};
+    const gameRoom = rooms[room];
 
+    if (!gameRoom) return;
 
+    gameRoom.players = giveRoles(gameRoom.players);
 
-res.json({
+    createGame(room, gameRoom.players);
 
-code:code
+    gameRoom.players.forEach(player => {
+      if (player.id) {
+        io.to(player.id).emit("yourRole", {
+          role: player.role.name
+        });
+      }
+    });
+
+    io.to(room).emit("phase", "night");
+
+  });
+
+  socket.on("nightAction", (data) => {
+    nightAction(data.room, socket.id, data.action);
+  });
+
+  socket.on("startDay", (room) => {
+    startDay(room);
+    io.to(room).emit("phase", "day");
+  });
+
+  socket.on("vote", (data) => {
+    vote(data.room, socket.id, data.target);
+  });
+
+  socket.on("endVote", (room) => {
+    const loser = getWinner(room);
+    io.to(room).emit("votedOut", loser);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Игрок вышел", socket.id);
+  });
 
 });
 
-
-});
-
-
-
-
-
-io.on("connection",(socket)=>{
-
-
-console.log(
-"Игрок подключился",
-socket.id
-);
-
-
-
-
-
-socket.on("joinRoom",(data)=>{
-
-
-const room=data.room;
-
-
-if(!rooms[room]){
-
-rooms[room]={
-
-players:[],
-
-status:"waiting"
-
-};
-
-}
-
-
-
-rooms[room].players.push({
-
-id:socket.id,
-
-name:data.name,
-
-role:null,
-
-alive:true
-
-});
-
-
-
-socket.join(room);
-
-
-
-io.to(room).emit(
-"players",
-rooms[room].players.map(p=>p.name)
-);
-
-
-});
-
-
-
-
-
-
-
-socket.on("startGame",(room)=>{
-
-
-const gameRoom=rooms[room];
-
-
-if(!gameRoom)
-return;
-
-
-
-gameRoom.players =
-giveRoles(gameRoom.players);
-
-
-
-createGame(
-room,
-gameRoom.players
-);
-
-
-
-gameRoom.players.forEach(player=>{
-
-
-if(player.id){
-
-io.to(player.id).emit(
-"yourRole",
-{
-role:player.role.name
-}
-);
-
-}
-
-
-});
-
-
-
-io.to(room).emit(
-"phase",
-"night"
-);
-
-
-
-});
-
-
-
-
-
-
-
-socket.on("nightAction",(data)=>{
-
-
-nightAction(
-data.room,
-socket.id,
-data.action
-);
-
-
-});
-
-
-
-
-
-
-
-socket.on("startDay",(room)=>{
-
-
-startDay(room);
-
-
-io.to(room).emit(
-"phase",
-"day"
-);
-
-
-});
-
-
-
-
-
-
-
-socket.on("vote",(data)=>{
-
-
-vote(
-data.room,
-socket.id,
-data.target
-);
-
-
-});
-
-
-
-
-
-
-
-socket.on("endVote",(room)=>{
-
-
-const loser =
-getWinner(room);
-
-
-io.to(room).emit(
-"votedOut",
-loser
-);
-
-
-});
-
-
-
-
-
-
-
-socket.on("disconnect",()=>{
-
-
-console.log(
-"Игрок вышел",
-socket.id
-);
-
-
-});
-
-
-});
-
-
-
-
-
-
-
-server.listen(
-process.env.PORT || 3000,
-()=>{
-
-console.log(
-"🎭 Mafiai server online"
-);
-
+server.listen(process.env.PORT || 3000, () => {
+  console.log("🎭 Mafiai server online");
 });
